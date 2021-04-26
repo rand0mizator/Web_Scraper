39491abd1e38b2585183b0c5b3b4d7e665be9b10
rand0mizator <iosuslov@gmail.com>
2021-04-26 21:22:56 +0700

    Stage 3/5 saving content from input url to file "source.htm" in bytes format

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,19 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
-
-headers = {'Accept-Language': 'en-US,en;q=0.5'}
-output = dict()
 
 url = input()
+r = requests.get(url)
+status_code = r.status_code
 
-r = requests.get(url, headers=headers)
-html_doc = r.content
-soup = BeautifulSoup(html_doc, 'html.parser')
-p = soup.find('div', {'class': 'summary_text'})
-
-try:
-    output['title'] = soup.title.string
-    output['description'] = p.string
-    print(output)
-except AttributeError:
-    print('Invalid movie page!')
+if status_code != 200:
+    print(f'The URL returned {status_code}')
+else:
+    page_content = r.content
+    file = open('source.html', 'wb')
+    file.write(page_content)
+    file.close()
+    print("Content saved")
