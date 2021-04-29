b1a6047babcb086e6deb4227a8504ac46d39fd5d
Rand0mizator <iosuslov@gmail.com>
2021-04-29 09:11:38 +0700

    stage 4/6

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,13 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-data = {}
-
-url = 'https://www.nature.com/nature/articles'
-r = requests.get(url)
+urls = []
+starting_url = 'https://www.nature.com/nature/articles'
+r = requests.get(starting_url)
 status_code = r.status_code
 soup = BeautifulSoup(r.content, 'html.parser')
-all_headers = soup.find_all('h3')
+all_headers = soup.find_all(attrs={'data-track-action': 'view article'})
+print(all_headers)
 for header in all_headers:
-    print(f"{header.a.string} - https://www.nature.com{header.a['href']}")
+    urls.append(f"https://www.nature.com{header.get('href')}")
+print(urls)
 
+
+def get_title(url):
+    pass
+
+
+def get_content(url):
+    pass
+
+
+def prepare_content(text):
+    pass
+
+
+def save_file(file_name, content):
+    pass
