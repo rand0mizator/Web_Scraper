7d6e1e2439b9965b1b9cb27876cf24e2d99afebf
Rand0mizator <iosuslov@gmail.com>
2021-04-30 10:27:20 +0700

    stage 4/5 Get all articles URL with type "News" from 'https://www.nature.com/nature/articles/' Visit every URL and save its contents to file with file_name = article_title (spaces = undescores), text = arcticle_body (stripped)
    Used tags
    class_="c-meta__type" - article type (News)
    class_="article__body cleared" - article text
    attrs={'data-track-action': 'view article'} - article href

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -20,12 +20,8 @@ def get_article_title(page):
     parsed = BeautifulSoup(page, 'html.parser')
     print("Getting article title...")
     title = parsed.title.string
-    if title:
-        print("Done!")
-        return title
-    else:
-        print(f"Something went wrong, title = {title}")
-        return False
+    print("Done!")
+    return title
 
 
 def get_article_text(page):
@@ -34,13 +30,7 @@ def get_article_text(page):
     print("Getting article text...")
     text = parsed.find(class_="article__body cleared")
     print("Done!")
-    return text.get_text(strip=True)
-
-
-
-def prepare_text(article_text):
-    """strip all trailing whitespaces in article text"""
-    pass
+    return text.get_text()
 
 
 def prepare_file_name(title):
@@ -48,35 +38,38 @@ def prepare_file_name(title):
     (str.maketrans and string.punctuation will be useful for this). Also, strip all trailing whitespaces
      in the article title"""
     stripped_title = str(title).strip()
-    none_string = punctuation + '‘’'
-    mytable = stripped_title.maketrans(' ', '_', none_string)
-    return stripped_title.translate(mytable)
+    trans_table = stripped_title.maketrans(' ', '_', punctuation)  # swap "space" with "undescore", "punctuation" with None
+    return stripped_title.translate(trans_table) + '.txt'
 
 
-
-def save_file(article_title, article_text):
+def save_file(title, text):
     """saves file with article_title and article_text"""
-    pass
+    print("Saving to file")
+    file = open(title, 'w', encoding='utf-8')
+    file.write(text)
+    file.close()
+    print(f"File saved {title}")
 
 
 urls = []
-starting_url = 'https://www.nature.com/nature/articles?type=news'
+starting_url = 'https://www.nature.com/nature/articles/'
 r = get_page_content(starting_url)
 soup = BeautifulSoup(r, 'html.parser')
-all_headers = soup.find_all(attrs={'data-track-action': 'view article'})
-# print(all_headers)
-for header in all_headers:
-    urls.append(f"https://www.nature.com{header.get('href')}")
-# print(urls)
+all_articles = soup.find_all('article')
+for article in all_articles:
+    if article.find(class_="c-meta__type").string == 'News':
+        article_href = article.find(attrs={'data-track-action': 'view article'}).get('href')
+        urls.append(f"https://www.nature.com{article_href}")
 
+print(urls)
 for url in urls:
+    print("-------------------------------------------")
     page_content = get_page_content(url)
     article_title = get_article_title(page_content)
-    print(article_title)
-    article_text = get_article_text(page_content)
-    print(article_text)
+    article_text = str(get_article_text(page_content)).strip()
     file_name = prepare_file_name(article_title)
-    print(file_name)
+    save_file(file_name, article_text)
+
 
 
 
