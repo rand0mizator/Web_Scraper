ce73d4c5d291886a505443ebf814c3d61207edc4
rand0mizator <iosuslov@gmail.com>
2021-05-23 22:06:22 +0700

    Improve your code so that the function can take two parameters from the user input: the number of pages (an integer) and the type of articles (a string). The integer with the number of pages specifies the number of pages on which the program should look for the articles. Go back to the https://www.nature.com/nature/articles website and find out how to navigate between the pages with the requests module changing the URL. Create a directory named Page_N (where N is the page number) for each page in the desired category, and put all the articles that are found on the page with the matched type to this directory. Save the articles to separate *.txt files. Keep the same processing of the titles for the filenames as in the previous stage. You can give users some feedback on completion, but it is not required.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,4 +1,6 @@
 import requests
+import os
+
 from bs4 import BeautifulSoup
 from string import punctuation
 
@@ -19,8 +21,8 @@ def get_article_title(page):
     """return article title"""
     parsed = BeautifulSoup(page, 'html.parser')
     print("Getting article title...")
-    title = parsed.title.string
-    print("Done!")
+    title = parsed.find(class_="article-item__title").string
+    print(f"Done! {title}")
     return title
 
 
@@ -28,8 +30,10 @@ def get_article_text(page):
     """get article text"""
     parsed = BeautifulSoup(page, 'html.parser')
     print("Getting article text...")
-    text = parsed.find(class_="article__body cleared")
-    print("Done!")
+    text = parsed.find(class_="article__body u-clearfix")
+    if not text:
+        text = parsed.find(class_="article-item__body")
+    print(f"Done! {text}")
     return text.get_text()
 
 
@@ -51,24 +55,48 @@ def save_file(title, text):
     print(f"File saved {title}")
 
 
-urls = []
-starting_url = 'https://www.nature.com/nature/articles/'
-r = get_page_content(starting_url)
-soup = BeautifulSoup(r, 'html.parser')
-all_articles = soup.find_all('article')
-for article in all_articles:
-    if article.find(class_="c-meta__type").string == 'News':
-        article_href = article.find(attrs={'data-track-action': 'view article'}).get('href')
-        urls.append(f"https://www.nature.com{article_href}")
+def create_folder(page_number):
+    """creates a folder with Page_N name"""
+    print(f"Creating folder with Page_{page_number} name...")
+    try:
+        os.mkdir(f"Page_{page_number}")
+        print("Done!")
+    except FileExistsError:
+        print("Folder already exists, continue...")
+        return True
 
-print(urls)
-for url in urls:
-    print("-------------------------------------------")
-    page_content = get_page_content(url)
-    article_title = get_article_title(page_content)
-    article_text = str(get_article_text(page_content)).strip()
-    file_name = prepare_file_name(article_title)
-    save_file(file_name, article_text)
+
+def change_saving_directory(directory_name):
+    """changes current working directory to directory name"""
+    print(f"Changing working directory to {directory_name}...")
+    os.chdir(directory_name)
+    print(f"Done, we are in {os.getcwd()}")
+
+
+starting_url = 'https://www.nature.com/nature/articles/?searchType=journalSearch&sort=PubDate&page='
+page_count = int(input())
+article_type = input()
+base_folder = os.getcwd()
+
+for page_num in range(1, page_count + 1):
+    create_folder(page_num)
+    change_saving_directory(f"Page_{page_num}")
+    r = get_page_content(starting_url + str(page_num))
+    soup = BeautifulSoup(r, 'html.parser')
+    all_articles = soup.find_all('article')
+    for article in all_articles:
+        if article.find(class_="c-meta__type").string == article_type:
+            article_href = article.find(attrs={'data-track-action': 'view article'}).get('href')
+            url = f"https://www.nature.com{article_href}"
+            print("-------------------------------------------")
+            page_content = get_page_content(url)
+            article_title = get_article_title(page_content)
+            article_text = str(get_article_text(page_content)).strip()
+            file_name = prepare_file_name(article_title)
+            save_file(file_name, article_text)
+    change_saving_directory(base_folder)
+
+print("Saved all articles.")
 
 
 
